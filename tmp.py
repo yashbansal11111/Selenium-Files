0a2c7f821e14830d06d58d09b4a5dd8df8272f85
yash <bansal.yash111@gmail.com>
2022-06-03 15:19:11 +0530

    tested 50 working

--- tmp.py
+++ tmp.py
@@ -1,4 +1,6 @@
 import time as t
+import string
+import random
 from selenium import webdriver
 from selenium.common import exceptions
 import pandas as pd
@@ -11,29 +13,26 @@ browser.maximize_window()
 browser.get('https://chat.rootle.ai/')
 browser.implicitly_wait(3)
 
-all_sheets_dict = pd.read_excel('response_sheet.xlsx', sheet_name=None)
-reschedule_sheet = pd.read_excel("reschedule_sheet.xlsx")
-no_of_rows = all_sheets_dict['Sheet1'].index
-sent_replies = []
-reschedule_index_increment = 0
-writer = pd.ExcelWriter("response_with_status.xlsx", engine = 'openpyxl', mode = 'a', if_sheet_exists = 'overlay') # pylint: disable=abstract-class-instantiated
-reschedule_writer = pd.ExcelWriter("reschedule_sheet.xlsx", engine = 'openpyxl', mode = 'a', if_sheet_exists = 'overlay') # pylint: disable=abstract-class-instantiated
+all_sheets_dict = pd.read_excel('response_sheet_small.xlsx', sheet_name=None)
+reschedule_sheet = pd.read_excel("reschedule_sheet_small.xlsx")
+no_of_rows_range = all_sheets_dict['Sheet1'].index
+len_rows = len(no_of_rows_range)
 
 #Check greeting message on the basis of time-stamp.
-time_stamp_flag = False
-greeting_msg = browser.find_element_by_class_name('msg-text').text
-am_or_pm = browser.find_element_by_class_name('time-stamp').text.split(' ')
-if am_or_pm[1] == 'pm':
-    time = int(am_or_pm[0].split(':')[0]) + 12
-    if time>=11 and time<=17 or time == 24:
-        if greeting_msg in ['Hello John, Very Good afternoon.', 'Hello John, Good afternoon.']:
-            time_stamp_flag = True
-    if time>=18 and time<=23:
-        if greeting_msg in ['Hello John, Very Good evening.', 'Hello John, Good evening.']:
-            time_stamp_flag=True
-elif am_or_pm[1] == 'am':
-    if greeting_msg in ['Hello John, Very Good morning.', 'Hello John, Good morning.']:
-        time_stamp_flag = True
+# time_stamp_flag = False
+# greeting_msg = browser.find_element_by_class_name('msg-text').text
+# am_or_pm = browser.find_element_by_class_name('time-stamp').text.split(' ')
+# if am_or_pm[1] == 'pm':
+#     time = int(am_or_pm[0].split(':')[0]) + 12
+#     if time>=11 and time<=17 or time == 24:
+#         if greeting_msg in ['Hello John, Very Good afternoon.', 'Hello John, Good afternoon.']:
+#             time_stamp_flag = True
+#     if time>=18 and time<=23:
+#         if greeting_msg in ['Hello John, Very Good evening.', 'Hello John, Good evening.']:
+#             time_stamp_flag=True
+# elif am_or_pm[1] == 'am':
+#     if greeting_msg in ['Hello John, Very Good morning.', 'Hello John, Good morning.']:
+#         time_stamp_flag = True
 
 regards_list = ["Thank you for your time and consideration; our HR staff will contact you for a follow-up interview.",
                 "Thank you for your time and consideration; our Human Resources staff will contact you to schedule a follow-up interview.",
@@ -56,7 +55,7 @@ pass_question_list = ['Hello John, Very Good afternoon.',
                     'Hello John, Good afternoon.',
                     'Hello John, Good evening.',
                     'I am calling from Dell Technologies. Is it good moment to talk?',
-                    'I am calling from Dell Technologies. Is it right time to talk?',
+                    'I am phoning from Dell Technologies. Is it right time to talk?',
                     "We're looking for a Principal Software Engineer-IT to join our team based in Hyderabad. Are you interested?",
                     "We are searching for a Principal Software Engineer-IT to join our team in Hyderabad. Are you interested?",
                     "Principal Software Engineer-IT is a position that we are looking to fill for Hyderabad location. Are you interested?",
@@ -78,14 +77,20 @@ pass_question_list = ['Hello John, Very Good afternoon.',
                     "Thank you for your time and consideration; our Human Resources staff will contact you for a follow-up interview."
                     ]
 
+
 #Looping through all the sheets by rows.
-if time_stamp_flag:
-    for i in range(len(no_of_rows)):
+def automation_script(status_file, reschedule_file, start_row_number = 0, end_row_number = len_rows):
+    sent_replies = []
+    reschedule_index_increment = 0
+    writer = pd.ExcelWriter(status_file, engine = 'openpyxl', mode = 'a', if_sheet_exists = 'overlay') # pylint: disable=abstract-class-instantiated
+    reschedule_writer = pd.ExcelWriter(reschedule_file, engine = 'openpyxl', mode = 'a', if_sheet_exists = 'overlay') # pylint: disable=abstract-class-instantiated
+    # if time_stamp_flag:
+    for i in range(start_row_number, end_row_number):
         print(i,'++++++++++')
-        #count = 0
         result_df = pd.DataFrame(columns=['Questions','Replies'])
         flag = False
         tmp = 1
+        conv_no = 1
         chat_history_list = []
 
         for k,v in all_sheets_dict.items():
@@ -95,22 +100,29 @@ if time_stamp_flag:
                 reply_field.send_keys(k.iloc[i,1])
                 reply_button = browser.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div/button')
                 reply_button.click()
-                t.sleep(1)
-                sent_replies.append(k.iloc[i,1])
-
-                first_ques = browser.find_element_by_xpath("/html/body/div/div/div/div[1]/div[2]/div[1]/div/div/span").text
-                all_element_list = browser.find_elements_by_class_name("msg-text")
+                t.sleep(0.5)
                 
+                sent_replies.append(k.iloc[i,1])
+                t.sleep(5)
+                #random_string = str.join('', random.choices(string.ascii_letters, k=100))
+                first_ques = browser.find_element_by_xpath("/html/body/div/div/div/div[1]/div[2]/div[1]/div/div/span").text
+                #random_and_first_ques = random_string+first_ques
+                all_element_list = browser.find_elements_by_class_name("msg-text")
+
                 for chat in all_element_list:
                     current_chat = chat.text
-
+                # if first_ques in current_chat:
+                #     print('its working ///////////////////////')
+                #     t.sleep(0.5)
+                #     k.iloc[i,2] = 'Repeated'
+                #     flag = True
                 if current_chat in gibberish_question_list:
                     k.iloc[i,2] = 'Failed'
                     flag = True
                 elif current_chat == first_ques or current_chat in chat_history_list:
+                    t.sleep(0.5)
                     k.iloc[i,2] = 'Repeated'
                     flag=True
-                    break
                 elif current_chat in pass_question_list:
                     k.iloc[i,2] = 'Passed'
                 elif current_chat in reschedule_question_list:
@@ -120,7 +132,7 @@ if time_stamp_flag:
                         reply_field.send_keys(reschedule_sheet.iloc[j+reschedule_index_increment,1])
                         reply_button = browser.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div/button')
                         reply_button.click()
-
+                        t.sleep(5)
                         all_element_list_for_rescheduling = browser.find_elements_by_class_name("msg-text")
                         
                         for chat in all_element_list_for_rescheduling:
@@ -135,6 +147,15 @@ if time_stamp_flag:
                         reschedule_index_increment = reschedule_index_increment + 1
                         flag = True
                         break
+                else:
+                    t.sleep(1)
+                    k.iloc[i,2] = 'Failed/Repeated'
+                    flag = True
+                # elif chat_history_list[-1] in current_chat:
+                #     print('its working for further conversation ///////////////////////')
+                #     t.sleep(0.5)
+                #     k.iloc[i,2] = 'Repeated'
+                #     flag = True
                 print(k)
                 k.to_excel(writer, sheet_name=f'status_sheet{tmp}', index=False)
                 tmp = tmp + 1
@@ -149,9 +170,9 @@ if time_stamp_flag:
 
         chats = browser.find_elements(By.XPATH, "//span[contains(@class,'msg-text')]")
         datetime_stamp = datetime.datetime.now()
-        edited = "Timestamp: %s-%s-%s %s:%s:%s" % (datetime_stamp.year, datetime_stamp.month, datetime_stamp.day, datetime_stamp.hour, datetime_stamp.minute, datetime_stamp.second )
-        question_list = []
-        reply_list = ['','']
+        timestamp = "Timestamp: %s-%s-%s %s:%s:%s" % (datetime_stamp.year, datetime_stamp.month, datetime_stamp.day, datetime_stamp.hour, datetime_stamp.minute, datetime_stamp.second )
+        question_list = [conv_no,timestamp]
+        reply_list = ['','','']
         for item in chats:
             chat_in_textformat = item.text
             if chat_in_textformat not in sent_replies:
@@ -166,16 +187,11 @@ if time_stamp_flag:
         result_df['Questions'] = pd.Series(question_list)
         result_df['Replies'] = pd.Series(reply_list)
         result_df.to_csv('chat_results.csv', mode='a', index=False)
-
-        # zip_obj = zip(question_list,reply_list)
-        # zipped_list = list(zip_obj)
-        # chat_df =  pd.DataFrame(zipped_list, columns=['Questions','Replies'])
-        # print(chat_df)
-        # writer = pd.ExcelWriter("results.xlsx" , engine='openpyxl', mode='a' ) # pylint: disable=abstract-class-instantiated
-        # chat_df.to_excel(writer, sheet_name='Results')
-        # writer.save()
-
+        conv_no = conv_no + 1
         browser.implicitly_wait(2)
         browser.refresh()
-else:
-    print('There seems to be an error in TimeStamp')
+    # else:
+    #     print('There seems to be an error in TimeStamp')
+
+
+automation_script("response_with_status_temp.xlsx", "reschedule_sheet_small.xlsx", len_rows-13,len_rows-12)
